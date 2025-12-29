35d95cf75980fffa6c0f670a0bf553351eefa20e
Matheus Mendes <mendes.souza.mat@gmail.com>
2025-12-29 03:05:58 -0300

    event-form component

--- tecboard/src/App.jsx
+++ tecboard/src/App.jsx
@@ -1,5 +1,17 @@
 import './App.css'
 
+function EventForm() {
+  return (
+    <form className="event-form">
+      <h2>Preencha para criar um evento:</h2>
+      <fieldset>
+        <label htmlFor="eventTitle">Título do Evento:</label>
+        <input type="text" id="eventTitle"/>
+      </fieldset>
+    </form>
+  )
+}
+
 function App() {
   return (
     <main>
@@ -9,6 +21,7 @@ function App() {
       <section>
         <img src="/banner.png" alt="banner"/>
       </section>
+      <EventForm/>
     </main>
   )
 }
